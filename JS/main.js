baed703dcc9b376c8ea059e0e06a89a333570117
nico-rilo <nicolasrilo@live.com.ar>
2022-08-02 23:23:48 -0300

    modificacion en JS/main.js

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,11 @@
+let formulario = document.getElementById("formulario");
+let inversion = document.getElementById("inversion");
+let boton = document.getElementById("calcular");
+let contenedor = document.getElementById("contenedor");
+let rangoInversion
+
 let precio = 0;
-
 const intereses = [1.1, 1.2, 1.3, 1.4, 1.5]
-
 const PlazosFijos = [];
 
 class plazoFijoUsuario {
@@ -12,47 +16,67 @@ class plazoFijoUsuario {
     }
 }
 
+
 function pedirMonto() {
-    return parseFloat (prompt("Calcula tu Plazo Fijo: Ingresá el monto, hasta $1000 - 10% anual, hasta $2000 - 20% anual, hasta $3000 - 30% anual, hasta $4000 - 40% anual, y superior a $5000 - 50% anual. RECORDÁ QUE EL DEPÓSITO NO PUEDE SUPERAR LOS $10000"));
-}
+    return parseFloat (prompt("SIMULADOR DE PLAZO FIJO: Ingresá el monto a depositar, hasta $1000 tu interés será del 10% anual, hasta $2000 tu interés será del 20% anual, hasta $3000 tu interés será del 30% anual, hasta $4000 tu interés será del 40% anual, y depósitos superiores a $5000 tu interés será del 50% anual. RECORDÁ QUE EL DEPÓSITO NO PUEDE SUPERAR LOS $10000")); 
     
-function CalcularIntereses() {
+}
+
+
+const validarCampos = () => {
+    if (!inversion.value) {
+        alert("Debe llenar el campo con números");
+        return false;
+    } else {
+        return true;
+    };
+}
+
+
+const crearTarjeta = () => {
+    
     precio = pedirMonto()
 
     while (!precio) {
         alert ("No es un dato válido. Intentá de nuevo: ");
         precio = pedirMonto();
     }
-
     while (precio > 10000) {
         alert ("No podés depositar mas de 10000. Intentá de nuevo: ");
         precio = pedirMonto();
-    }
-    
+    }    
     if (precio <= 1000) {
-        alert ("Depositando ese monto tenés el 10% de interés anual");
-        alert ("Tu depósito se convertirá en" + "$" + precio * intereses [0]);        
+        rangoInversion = ("Depositando ese monto tenés el 10% de interés anual, por lo tanto tu depósito se convertirá en" + "$" + precio * intereses [0]);        
         PlazosFijos.push(new plazoFijoUsuario(precio, intereses[0]));
+
     } else if (precio <= 2000) {
-        alert ("Depositando ese monto tenés el 20% de interés anual");
-        alert ("Tu depósito se convertirá en" + "$" + precio * intereses [1])
+        rangoInversion = ("Depositando ese monto tenés el 20% de interés anual, por lo tanto tu depósito se convertirá en" + "$" + precio * intereses [1]);        
         PlazosFijos.push(new plazoFijoUsuario(precio, intereses[1]));
+
     } else if (precio <= 3000) {
-        alert ("Depositando ese monto tenés el 30% de interés anual");
-        alert ("Tu depósito se convertirá en" + "$" + precio * intereses [2])
+        rangoInversion = ("Depositando ese monto tenés el 30% de interés anual, por lo tanto tu depósito se convertirá en" + "$" + precio * intereses [2]);        
         PlazosFijos.push(new plazoFijoUsuario(precio, intereses[2]));
+
     } else if (precio <= 4000) {
-        alert ("Depositando ese monto tenés el 40% de interés anual");
-        alert ("Tu depósito se convertirá en" + "$" + precio * intereses [3])
+        rangoInversion = ("Depositando ese monto tenés el 40% de interés anual, por lo tanto tu depósito se convertirá en" + "$" + precio * intereses [3]);        
         PlazosFijos.push(new plazoFijoUsuario(precio, intereses[3]));
-    } else { alert ("Depositando ese monto tenés el 50% de interés anual"); 
-        alert ("Tu depósito se convertirá en" + "$" + precio * intereses [4]);
+
+    } else { rangoInversion = ("Depositando ese monto tenés el 50% de interés anual, por lo tanto tu depósito se convertirá en" + "$" + precio * intereses [4]);        
     PlazosFijos.push(new plazoFijoUsuario(precio, intereses[4]));
     }
+  
+
+    contenedor.innerHTML += `<div class="card col-2 mx-1">
+                                <h3> Con tus ${inversion.value} de inversión </h3>
+                                <p> ${rangoInversion}.</p>
+                            </div>`;
+
+    formulario.reset();
 
 }
 
-function repetirCiclo() {
+
+/* function repetirCiclo() {
     let respuesta;
     do {
         respuesta = prompt("Querés agregar otro plazo fijo? Respondé sólo si o no") .toLowerCase();
@@ -65,9 +89,18 @@ function repetirCiclo() {
             alert ("No pusiste un si o un no. Intentá nuevamente: ");
         }
     } while (respuesta !== "si" && respuesta !== "no");
-}
+} */
 
-function mostrarPlazos() {
+
+boton.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (validarCampos()) {
+        crearTarjeta()
+    };
+});
+
+
+/* function mostrarPlazos() {
     alert ("Estos son tus plazos fijos: ");
     mensaje = "";
     for(const plazoFijo of PlazosFijos) {
@@ -81,37 +114,4 @@ repetirCiclo();
 mostrarPlazos();
 
 const totalSuma = PlazosFijos.reduce((acc, el) => + acc + el.deposito, 0)
-alert ("Tu Saldo final con depósitos e intereses será de: $ " + totalSuma) 
-
-
-
-
-const titulo = (document.querySelector("h1").textContent = "CrediMas");
-
-const subTitulo = (document.querySelector(".text-white-50").textContent = "Es, mejor.");
-
-const titulo2 = (document.querySelector("h2").textContent = "La vida sin problemas es matar el tiempo a lo bobo");
-
-const descripcion = (document.querySelector(".lead").textContent = "Happiness is a warm gun");
-
-const enlace = document.querySelector(".navbar-brand");
-enlace.remove()
-
-let agregado = document.createElement("p");
-agregado.innerHTML = "<h5>Parrafo agregados</h5>";
-
-const encabezado = document.querySelector(".encabezado");
-console.log (encabezado.children)
-
-encabezado.insertBefore(agregado, encabezado[1]); 
-
-/* agrego array */
-let listaVacia = document.querySelector("#listaVacia");
-
-let otrosServicios = ["Dólares", "Bonos", "Caja de seguridad"];
-
-for (let servicio of otrosServicios) {
-    let listado = document.createElement("li");
-    listado.innerHTML = servicio;
-    listaVacia.appendChild(listado);
-    }
+alert ("Tu Saldo final con depósitos e intereses será de: $ " + totalSuma)  */
